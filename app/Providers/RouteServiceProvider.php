ca8070f5399b49689509f849f7c9818476d6ef5b
20tk11 <tomaskanapickass@gmail.com>
2022-05-01 04:40:58 +0300

    Created a php base web app

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,14 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-
+    /**
+    * This namespace is applied to your controller routes.
+    *
+    * In addition, it is set as the URL generator's root namespace.
+    *
+    * @var string
+    */
+    protected $namespace = 'App\Http\Controllers';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
@@ -31,9 +38,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }

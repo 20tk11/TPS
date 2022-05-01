d44e2c0e5d422a3086d122145581711ea262d2ca
20tk11 <tomaskanapickass@gmail.com>
2022-05-01 07:19:22 +0300

    Added Calendar view

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', 'HomeController@welcome');
 Route::get('/signin', 'AuthController@signin');
 Route::get('/callback', 'AuthController@callback');
 Route::get('/signout', 'AuthController@signout');
+Route::get('/calendar', 'CalendarController@calendar');
+Route::get('/calendar/new', 'CalendarController@getNewEventForm');
+Route::post('/calendar/new', 'CalendarController@createNewEvent');
